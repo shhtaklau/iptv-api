03700b7a54d016770d17f1a2f630341a8a3d7915
shhtaklau <shhtaklau@hotmail.com>
2026-03-11 12:38:17 +0800

    Update rihou.py

--- rihou/rihou.py
+++ rihou/rihou.py
@@ -12,6 +12,9 @@ def rihou_playlist_m3u(url=url, output="playlist.m3u"):
     with open(f'rihou{date}.txt', 'w', encoding='utf-8')as f1:
         f1.write(txt)
 
+    with open(f'rihou.txt', 'w', encoding='utf-8')as f3:
+        f3.write(txt)
+
     # 转换并保存
     with open(f'rihou{date}.m3u', 'w', encoding='utf-8') as f2:
         f2.write("#EXTM3U\n")
@@ -24,6 +27,18 @@ def rihou_playlist_m3u(url=url, output="playlist.m3u"):
                 name, url = line.split(",", 1)
                 group_part = f' group-title="{group}"' if group else ''
                 f2.write(f'#EXTINF:-1 tvg-name="{name}"{group_part},{name}\n{url}\n')
+
+    with open(f'rihou.m3u', 'w', encoding='utf-8') as f4:
+        f4.write("#EXTM3U\n")
+        group = ""
+        for line in txt.splitlines():
+            line = line.strip()
+            if line.endswith(",#genre#"):
+                group = line.split(",")[0]
+            elif line and "," in line and not line.endswith("#genre#"):
+                name, url = line.split(",", 1)
+                group_part = f' group-title="{group}"' if group else ''
+                f4.write(f'#EXTINF:-1 tvg-name="{name}"{group_part},{name}\n{url}\n')
 
     print(f"✓ 完成: {output}")
 
